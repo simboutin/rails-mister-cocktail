52662693b131e003e5e0bbcfcce1d654d0cdcc05
Simon Boutin <simboutin@gmail.com>
2021-01-23 14:22:43 +0100

    Setup models/controllers/views Cocktails & Doses

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,29 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+    @doses = @cocktail.doses
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(params_cocktail)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def params_cocktail
+    params.require(:cocktail).permit(:name)
+  end
+end
---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,32 @@
+class DosesController < ApplicationController
+  def index
+    @doses = Dose.all
+  end
+
+  def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def create
+    @dose = Dose.new(params_dose)
+    @dose.cocktail = Cocktail.find(params[:cocktail_id])
+    if @dose.save
+      redirect_to cocktail_path(@dose.cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  private
+
+  def params_dose
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
